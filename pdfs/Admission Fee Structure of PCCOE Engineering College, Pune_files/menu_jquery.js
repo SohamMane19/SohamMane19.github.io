df31b0c7ee9a93a162df6a2781e85ab6c2359511
ssmane <[ssmane@gmail.com]>
2023-07-27 20:36:22 +0530

    tweaked the pdf link's

---
+++ pdfs/Admission Fee Structure of PCCOE Engineering College, Pune_files/menu_jquery.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#cssmenu").prepend('<div id="menu-button">Menu</div>'),$("#cssmenu #menu-button").on("click",function(){var n=$(this).next("ul");n.hasClass("open")?n.removeClass("open"):n.addClass("open")})});  
